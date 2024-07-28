18f647fe995aeaef52a7d0a7714d24c45abf8a54
Hollandcm1 <chris_h1996@hotmail.com>
2024-07-28 15:55:02 -0300

    Update to combined figures

--- scripts/Combined Figures.R
+++ scripts/Combined Figures.R
@@ -20,6 +20,11 @@ tab_model(model)
 source(here('scripts', 'Figure_Parameters.R'))
 
 dodge_width <- 0.2 # seperation between conditions
+jitter = 0.4
+alpha_ribbon = 0.1
+alpha_points = 0.3
+point_size = 0.75
+ribbon_line_width = 0.3
 
 # Create Summary Data For Next Figures
 summary_data <- data_long %>% # calculate trust
@@ -29,9 +34,9 @@ summary_data <- data_long %>% # calculate trust
 
 g1 <- ggplot(summary_data, aes(x = Block, y = Average_Performance_Before, group = Condition, color = Condition, fill = Condition)) +
   geom_line() +
-  geom_point(position = position_dodge(width = dodge_width)) +
+  #geom_point(position = position_dodge(width = dodge_width), alpha = alpha_points) +
   geom_ribbon(aes(ymin = Average_Performance_Before - se, ymax = Average_Performance_Before + se),
-              alpha = 0.25, position = position_dodge(width = dodge_width)) +
+              alpha = alpha_ribbon, position = position_dodge(width = dodge_width), linewidth = ribbon_line_width) +
   theme_classic() +
   scale_color_manual(values = c("Decreasing" = colour.decreasing, "Increasing" = colour.increasing)) +
   scale_fill_manual(values = c("Decreasing" = colour.decreasing, "Increasing" = colour.increasing)) +
@@ -42,7 +47,8 @@ g1 <- ggplot(summary_data, aes(x = Block, y = Average_Performance_Before, group 
   ylim(0, 1) + 
   scale_x_continuous(breaks = seq(1, 6, 1)) +
   geom_point(data = data_long, aes(x = Block, y = Performance_Before, color = Condition), 
-             position = position_jitterdodge(jitter.width = 0.2, dodge.width = dodge_width), alpha = 0.5) +
+             position = position_jitterdodge(jitter.width = jitter, dodge.width = dodge_width), alpha = alpha_points,
+             size = point_size) +
   theme(plot.title = element_text(size=title_size)) + 
   # remove legend
   theme(legend.position = "none")
@@ -57,9 +63,9 @@ summary_data <- data_long %>% # calculate trust
 
 g2 <- ggplot(summary_data, aes(x = Block, y = Average_Performance_After, group = Condition, color = Condition, fill = Condition)) +
   geom_line() +
-  geom_point(position = position_dodge(width = dodge_width)) +
+  #geom_point(position = position_dodge(width = dodge_width)) +
   geom_ribbon(aes(ymin = Average_Performance_After - se, ymax = Average_Performance_After + se),
-              alpha = 0.25, position = position_dodge(width = dodge_width)) +
+              alpha = alpha_ribbon, position = position_dodge(width = dodge_width), linewidth = ribbon_line_width) +
   theme_classic() +
   scale_color_manual(values = c("Decreasing" = colour.decreasing, "Increasing" = colour.increasing)) +
   scale_fill_manual(values = c("Decreasing" = colour.decreasing, "Increasing" = colour.increasing)) +
@@ -70,7 +76,8 @@ g2 <- ggplot(summary_data, aes(x = Block, y = Average_Performance_After, group =
   ylim(0, 1) + 
   scale_x_continuous(breaks = seq(1, 6, 1)) +
   geom_point(data = data_long, aes(x = Block, y = Performance_After, color = Condition), 
-             position = position_jitterdodge(jitter.width = 0.2, dodge.width = dodge_width), alpha = 0.5) +
+             position = position_jitterdodge(jitter.width = jitter, dodge.width = dodge_width), alpha = alpha_points,
+             size = point_size) +
   theme(plot.title = element_text(size=title_size))
 print(g2)
 
@@ -83,8 +90,78 @@ print(g3)
 g4 <- ggarrange(g1, g2, ncol = 2, nrow = 1, widths = c(1, 1.5))
 print(g4)
 
+#save figure
+ggsave(here('output','figures','99_Performance_Before_and_After_by_Block_by_Condition_with_SE_as_Ribbon.png'), 
+       plot = g4, device = device, width = width, height = height, units = units, dpi = dpi)
 
 
 
+# Create Summary Data For Next Figures
+summary_data <- data_long %>% # calculate trust
+  group_by(Condition, Block) %>%
+  summarise(Average_Trust = mean(Trust, na.rm = TRUE),
+            se = sd(Trust, na.rm = TRUE) / sqrt(n()))  # Standard Error
+
+# Visualize Interaction with Standard Error as Ribbon
+dodge_width <- 0.2 # seperation between conditions
+g5 <- ggplot(summary_data, aes(x = Block, y = Average_Trust, group = Condition, color = Condition, fill = Condition)) +
+  geom_line() +
+  #geom_point(position = position_dodge(width = dodge_width)) +
+  geom_ribbon(aes(ymin = Average_Trust - se, ymax = Average_Trust + se),
+              alpha = alpha_ribbon, position = position_dodge(width = dodge_width), linewidth = ribbon_line_width) +
+  theme_classic() +
+  scale_color_manual(values = c("Decreasing" = colour.decreasing, "Increasing" = colour.increasing)) +
+  scale_fill_manual(values = c("Decreasing" = colour.decreasing, "Increasing" = colour.increasing)) +
+  labs(title = "Trust",
+       x = "Block",
+       y = "Trust",
+       color = "Condition") +
+  ylim(0, 100) + 
+  scale_x_continuous(breaks = seq(1, 6, 1)) +
+  geom_point(data = data_long, aes(x = Block, y = Trust, color = Condition), 
+             position = position_jitterdodge(jitter.width = jitter, dodge.width = dodge_width), alpha = alpha_points,
+             size = point_size) +
+  theme(plot.title = element_text(size=title_size)) +
+  # remove legend
+  theme(legend.position = "none")
+print(g5)
+
+
+# Create Summary Data For Next Figures
+summary_data <- data_long %>% # calculate trust
+  group_by(Condition, Block) %>%
+  summarise(Average_Confidence = mean(Confidence, na.rm = TRUE),
+            se = sd(Confidence, na.rm = TRUE) / sqrt(n()))  # Standard Error
+
+
+# Visualize Interaction with Standard Error as Ribbon
+dodge_width <- 0.2 # seperation between conditions
+g6 <- ggplot(summary_data, aes(x = Block, y = Average_Confidence, group = Condition, color = Condition, fill = Condition)) +
+  geom_line() +
+  #geom_point(position = position_dodge(width = dodge_width)) +
+  geom_ribbon(aes(ymin = Average_Confidence - se, ymax = Average_Confidence + se),
+              alpha = alpha_ribbon, position = position_dodge(width = dodge_width), linewidth = ribbon_line_width) +
+  theme_classic() +
+  scale_color_manual(values = c("Decreasing" = colour.decreasing, "Increasing" = colour.increasing)) +
+  scale_fill_manual(values = c("Decreasing" = colour.decreasing, "Increasing" = colour.increasing)) +
+  labs(title = "Self-Confidence",
+       x = "Block",
+       y = "Self-Confidence",
+       color = "Condition") +
+  ylim(0, 100) + 
+  scale_x_continuous(breaks = seq(1, 6, 1)) +
+  geom_point(data = data_long, aes(x = Block, y = Confidence, color = Condition), 
+             position = position_jitterdodge(jitter.width = jitter, dodge.width = dodge_width), alpha = alpha_points,
+             size = point_size) +
+  theme(plot.title = element_text(size=title_size))
+print(g6)
+
+# same figure but give more horizontal space to g2
+g7 <- ggarrange(g5, g6, ncol = 2, nrow = 1, widths = c(1, 1.5))
+print(g7)
+
+#save figure
+ggsave(here('output','figures','99_Trust_and_Confidence_by_Block_by_Condition_with_SE_as_Ribbon.png'), 
+       plot = g7, device = device, width = width, height = height, units = units, dpi = dpi)
 
 
